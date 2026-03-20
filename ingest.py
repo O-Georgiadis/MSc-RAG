7fae4eab0115b73867bf99a9e17037db29818ac3
O-Georgiadis <odysseas.georgiadis.98@gmail.com>
2026-03-20 23:17:33 +0200

    chunks addition

--- ingest.py
+++ ingest.py
@@ -1,5 +1,9 @@
 from pathlib import Path
 from unstructured.partition.pdf import partition_pdf
+from dotenv import load_dotenv
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 # PDF-to-text extractor
@@ -20,3 +24,27 @@ for pdf_file in data_path.glob("*.pdf"):
 
     # print(f"{pdf_file.name}: {len(elements)} elements")
 
+load_dotenv(override=True)
+
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+# embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+
+def create_chunks(all_documents):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=200
+    )
+    chunks = []
+    for doc in all_documents:
+        for chunk in text_splitter.split_text(doc["text"]):
+            chunks.append({
+                "chunk_text": chunk,
+                "source": doc["source"],
+                "type": doc["type"]
+            })
+
+    print(f"Created {len(chunks)} chunks for embeddings")
+    return chunks
+
+chunks = create_chunks(all_documents)
